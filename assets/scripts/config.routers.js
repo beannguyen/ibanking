6ae4b2275b45803a292cb3daa5b34d5f0c5cd27f
KhoaVo1994 <votrandangkhoa1710@gmail.com>
2015-05-22 21:51:40 +0700

    Khoa Vo

--- assets/scripts/config.routers.js
+++ assets/scripts/config.routers.js
@@ -28,7 +28,6 @@ angular.module('ibanking-app')
 		            	url: '/nap-tien',
 		            	templateUrl: 'assets/tpl/naptien.html'
 		            })
-<<<<<<< HEAD
 		            .state('app.chuyen-tien', {
 		            	url: '/chuyen-tien',
 		            	templateUrl: 'assets/tpl/transfer.html'
@@ -36,11 +35,10 @@ angular.module('ibanking-app')
 		            .state('app.rut-tien', {
 		            	url: '/rut-tien',
 		            	templateUrl: 'assets/tpl/rut-tien.html'
-=======
+		            })
 		            .state('app.nap-tien-bang-the', {
 		            	url: '/nap-tien-bang-the',
 		            	templateUrl: 'assets/tpl/napTienTheCao.html'
->>>>>>> 7fda1db53bcf6ee10ac7f353c45cfcb5544084d8
 		            })
 			}
 		]
